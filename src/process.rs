3f315c3e59d94e62874760fd2088d849dc855965
Alex Alikiotis <alexalikiotis5@gmail.com>
2022-09-09 16:26:05 +0300

    Refactoring process module

--- src/process.rs
+++ src/process.rs
@@ -46,27 +46,7 @@ pub fn initialize<'s>(
     set_property_to(scope, process, "argv", argv.into());
 
     // `process.cwd()` - current working directory.
-    set_function_to(
-        scope,
-        process,
-        "cwd",
-        |scope: &mut v8::HandleScope,
-         _args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            match env::current_dir() {
-                Ok(path) => {
-                    let path = path.into_os_string().into_string().unwrap();
-                    let path = v8::String::new(scope, path.as_str()).unwrap();
-
-                    rv.set(path.into());
-                }
-                Err(_) => {
-                    let undefined = v8::undefined(scope);
-                    rv.set(undefined.into());
-                }
-            }
-        },
-    );
+    set_function_to(scope, process, "cwd", cwd);
 
     // `process.env` - an object containing the user environment.
     let environment: Vec<(String, String)> = env::vars().collect();
@@ -80,46 +60,10 @@ pub fn initialize<'s>(
     set_property_to(scope, process, "env", env.into());
 
     // `process.exit([code])` - exits the program with the given code.
-    set_function_to(
-        scope,
-        process,
-        "exit",
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut _rv: v8::ReturnValue| {
-            // Exit the program when value is not valid i32.
-            match args.get(0).to_int32(scope) {
-                Some(code) => std::process::exit(code.value() as i32),
-                None => std::process::exit(0),
-            }
-        },
-    );
+    set_function_to(scope, process, "exit", exit);
 
     // `process.memoryUsage()` - an object describing the memory usage.
-    set_function_to(
-        scope,
-        process,
-        "memoryUsage",
-        |scope: &mut v8::HandleScope,
-         _args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            // Get HeapStatistics from v8.
-            let mut stats = v8::HeapStatistics::default();
-            scope.get_heap_statistics(&mut stats);
-
-            let total_heap = v8::Number::new(scope, stats.total_heap_size() as f64);
-            let used_heap = v8::Number::new(scope, stats.used_heap_size() as f64);
-            let external = v8::Number::new(scope, stats.external_memory() as f64);
-
-            let memory_usage = v8::Object::new(scope);
-
-            set_property_to(scope, memory_usage, "heapTotal", total_heap.into());
-            set_property_to(scope, memory_usage, "heapUsed", used_heap.into());
-            set_property_to(scope, memory_usage, "external", external.into());
-
-            rv.set(memory_usage.into());
-        },
-    );
+    set_function_to(scope, process, "memoryUsage", memory_usage);
 
     // `process.pid` - PID of the current process.
     let id = v8::Number::new(scope, std::process::id() as f64);
@@ -130,24 +74,7 @@ pub fn initialize<'s>(
     set_property_to(scope, process, "platform", platform.into());
 
     // `process.uptime()` - a number describing the amount of time (in seconds) the process is running.
-    set_function_to(
-        scope,
-        process,
-        "uptime",
-        |scope: &mut v8::HandleScope,
-         _args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            // Get access to runtime's state.
-            let state_rc = JsRuntime::state(scope);
-            let state = state_rc.borrow();
-
-            // Calculate uptime duration in seconds with millis precision.
-            let uptime = state.startup_moment.elapsed().as_millis() as f64 / 1000.0;
-            let uptime = v8::Number::new(scope, uptime);
-
-            rv.set(uptime.into());
-        },
-    );
+    set_function_to(scope, process, "uptime", uptime);
 
     // `process.version` - the dune version.
     let version = format!("v{}", VERSIONS.get("dune").unwrap());
@@ -166,73 +93,132 @@ pub fn initialize<'s>(
     set_property_to(scope, process, "versions", versions.into());
 
     // `process.kill` - sends the signal to the process identified by pid.
-    set_function_to(
-        scope,
-        process,
-        "kill",
-        |scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments, _: v8::ReturnValue| {
-            // Get PID and SIGNAL arguments
-            let pid = args.get(0).to_rust_string_lossy(scope);
-            let signal = args.get(1).to_rust_string_lossy(scope);
-
-            // Handle kill in UNIX platforms.
-            if cfg!(unix) {
-                // Check if the value is a valid NIX signal.
-                if nix::sys::signal::Signal::iterator()
-                    .map(|s| s.as_str())
-                    .find(|v| **v == signal)
-                    .is_none()
-                {
-                    throw_exception(scope, &format!("Invalid signal: {}", signal));
-                    return;
-                }
-                // Try to kill the process.
-                if let Err(e) = Command::new("kill")
-                    .args([&format!("-{}", signal), &pid])
-                    .output()
-                {
-                    throw_exception(scope, &e.to_string());
-                }
-            }
-
-            // Handle kill in the Windows platform.
-            if cfg!(not(unix)) {
-                // Note: In windows platform we'll ignore the signal argument.
-                if let Err(e) = Command::new("Taskkill").args(["/F", "/PID", &pid]).output() {
-                    throw_exception(scope, &e.to_string());
-                }
-            }
-        },
-    );
+    set_function_to(scope, process, "kill", kill);
 
     // `process.binding` - exposes native modules to JavaScript.
-    set_function_to(
-        scope,
-        process,
-        "binding",
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            // Get requested native binding.
-            let request = args.get(0).to_rust_string_lossy(scope);
-
-            match BINDINGS.get(request.as_str()) {
-                // Initialize binding.
-                Some(initializer) => {
-                    let binding = initializer(scope);
-                    let binding = v8::Local::new(scope, binding);
-                    rv.set(binding.into());
-                }
-                // Throw exception.
-                None => {
-                    let message = format!("No such module: \"{}\"", request);
-                    let message = v8::String::new(scope, &message).unwrap();
-                    let exception = v8::Exception::error(scope, message);
-                    scope.throw_exception(exception);
-                }
-            };
-        },
-    );
+    set_function_to(scope, process, "binding", bind);
 
     process
+}
+
+/// Current working directory.
+fn cwd(scope: &mut v8::HandleScope, _: v8::FunctionCallbackArguments, mut rv: v8::ReturnValue) {
+    match env::current_dir() {
+        Ok(path) => {
+            let path = path.into_os_string().into_string().unwrap();
+            let path = v8::String::new(scope, path.as_str()).unwrap();
+
+            rv.set(path.into());
+        }
+        Err(_) => {
+            let undefined = v8::undefined(scope);
+            rv.set(undefined.into());
+        }
+    }
+}
+
+/// Exits the program with the given code.
+fn exit(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments, _: v8::ReturnValue) {
+    // Exit the program when value is not valid i32.
+    match args.get(0).to_int32(scope) {
+        Some(code) => std::process::exit(code.value() as i32),
+        None => std::process::exit(0),
+    }
+}
+
+/// Returns an object describing the memory usage.
+fn memory_usage(
+    scope: &mut v8::HandleScope,
+    _: v8::FunctionCallbackArguments,
+    mut rv: v8::ReturnValue,
+) {
+    // Get HeapStatistics from v8.
+    let mut stats = v8::HeapStatistics::default();
+    scope.get_heap_statistics(&mut stats);
+
+    let total_heap = v8::Number::new(scope, stats.total_heap_size() as f64);
+    let used_heap = v8::Number::new(scope, stats.used_heap_size() as f64);
+    let external = v8::Number::new(scope, stats.external_memory() as f64);
+
+    let memory_usage = v8::Object::new(scope);
+
+    set_property_to(scope, memory_usage, "heapTotal", total_heap.into());
+    set_property_to(scope, memory_usage, "heapUsed", used_heap.into());
+    set_property_to(scope, memory_usage, "external", external.into());
+
+    rv.set(memory_usage.into());
+}
+
+/// A number describing the amount of time (in seconds) the process is running.
+fn uptime(
+    scope: &mut v8::HandleScope,
+    _args: v8::FunctionCallbackArguments,
+    mut rv: v8::ReturnValue,
+) {
+    // Get access to runtime's state.
+    let state_rc = JsRuntime::state(scope);
+    let state = state_rc.borrow();
+
+    // Calculate uptime duration in seconds with millis precision.
+    let uptime = state.startup_moment.elapsed().as_millis() as f64 / 1000.0;
+    let uptime = v8::Number::new(scope, uptime);
+
+    rv.set(uptime.into());
+}
+
+/// Sends the signal to the process identified by pid.
+fn kill(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments, _: v8::ReturnValue) {
+    // Get PID and SIGNAL arguments
+    let pid = args.get(0).to_rust_string_lossy(scope);
+    let signal = args.get(1).to_rust_string_lossy(scope);
+
+    // Kill process in UNIX platforms.
+    if cfg!(unix) {
+        // Check if the value is a valid NIX signal.
+        if nix::sys::signal::Signal::iterator()
+            .map(|s| s.as_str())
+            .find(|v| **v == signal)
+            .is_none()
+        {
+            throw_exception(scope, &format!("Invalid signal: {}", signal));
+            return;
+        }
+        // Try to kill the process.
+        if let Err(e) = Command::new("kill")
+            .args([&format!("-{}", signal), &pid])
+            .output()
+        {
+            throw_exception(scope, &e.to_string());
+        }
+    }
+
+    // Kill process in Windows platform.
+    if cfg!(not(unix)) {
+        // Note: In windows we'll ignore the signal argument.
+        if let Err(e) = Command::new("Taskkill").args(["/F", "/PID", &pid]).output() {
+            throw_exception(scope, &e.to_string());
+        }
+    }
+}
+
+/// Exposes native modules to JavaScript.
+fn bind(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments, mut rv: v8::ReturnValue) {
+    // Get requested native binding.
+    let request = args.get(0).to_rust_string_lossy(scope);
+
+    match BINDINGS.get(request.as_str()) {
+        // Initialize binding.
+        Some(initializer) => {
+            let binding = initializer(scope);
+            let binding = v8::Local::new(scope, binding);
+            rv.set(binding.into());
+        }
+        // Throw exception.
+        None => {
+            let message = format!("No such module: \"{}\"", request);
+            let message = v8::String::new(scope, &message).unwrap();
+            let exception = v8::Exception::error(scope, message);
+            scope.throw_exception(exception);
+        }
+    };
 }
