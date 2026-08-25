7a8bed1a674114ddab8895e1bd8cba24ec416454
Alex Alikiotis <alexalikiotis5@gmail.com>
2026-08-26 00:27:15 +0300

    Adding the read_start method in the tty binding

--- src/tty.rs
+++ src/tty.rs
@@ -1,8 +1,13 @@
 use crate::bindings::get_internal_ref;
+use crate::bindings::set_exception_code;
 use crate::bindings::set_function_to;
 use crate::bindings::throw_exception;
+use crate::runtime::JsFuture;
+use crate::runtime::JsRuntime;
+use anyhow::Result;
 use crabuv::tty::Mode;
 use crabuv::tty::TtyHandle;
+use std::rc::Rc;
 
 pub fn initialize(scope: &mut v8::PinScope) -> v8::Global<v8::Object> {
     // Create local JS object.
@@ -10,8 +15,74 @@ pub fn initialize(scope: &mut v8::PinScope) -> v8::Global<v8::Object> {
 
     set_function_to(scope, target, "isTTY", is_tty);
     set_function_to(scope, target, "setRawMode", set_raw_mode);
+    set_function_to(scope, target, "readStart", read_start);
 
     v8::Global::new(scope, target)
+}
+
+struct TTYReadFuture {
+    data: Result<Vec<u8>>,
+    on_read: Rc<v8::Global<v8::Function>>,
+}
+
+impl JsFuture for TTYReadFuture {
+    fn run(&mut self, scope: &mut v8::PinScope) {
+        // Create the v8 value for the data parameter.
+        let data: v8::Local<v8::Value> = match self.data.as_mut() {
+            Ok(data) => {
+                // Create ArrayBuffer's backing store from Vec<u8>.
+                let store = data.clone().into_boxed_slice();
+                let store = v8::ArrayBuffer::new_backing_store_from_boxed_slice(store);
+                let store = store.make_shared();
+
+                // Initialize ArrayBuffer.
+                let bytes = v8::ArrayBuffer::with_backing_store(scope, &store);
+                bytes.into()
+            }
+            Err(_) => v8::null(scope).into(),
+        };
+
+        // Create the v8 value for the error parameter.
+        let error: v8::Local<v8::Value> = match self.data.as_mut() {
+            Ok(_) => v8::null(scope).into(),
+            Err(e) => {
+                let message = v8::String::new(scope, &e.to_string()).unwrap();
+                let exception = v8::Exception::error(scope, message);
+                set_exception_code(scope, exception, e);
+                exception
+            }
+        };
+
+        // Get access to the on_read callback.
+        let on_read = v8::Local::new(scope, (*self.on_read).clone());
+        let undefined = v8::undefined(scope).into();
+
+        on_read.call(scope, undefined, &[error, data]);
+    }
+}
+
+/// Starts reading from a TTY stream.
+fn read_start(scope: &mut v8::PinScope, args: v8::FunctionCallbackArguments, _: v8::ReturnValue) {
+    // Get the tty wrapper object.
+    let tty = args.get(0).to_object(scope).unwrap();
+    let tty = get_internal_ref::<TtyHandle>(scope, tty, 0);
+
+    // Get reading callback.
+    let on_read = v8::Local::<v8::Function>::try_from(args.get(1)).unwrap();
+    let on_read = Rc::new(v8::Global::new(scope, on_read));
+
+    let state_rc = JsRuntime::state(scope);
+
+    // Start reading from the TTY stream.
+    tty.start_reading({
+        move |_: TtyHandle, data: Result<Vec<u8>>| {
+            let mut state = state_rc.borrow_mut();
+            let on_read = Rc::clone(&on_read);
+            let future = TTYReadFuture { data, on_read };
+
+            state.pending_futures.push(Box::new(future));
+        }
+    });
 }
 
 /// Sets the TTY to raw or normal mode based on the provided mode argument.
